5b1b696b069b447dbd961dbe4e196866310c2103
Cami-loiacono <you@example.com>
2026-03-21 22:22:50 -0300

    sigoooouuo

--- magios.c
+++ magios.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+//FUNCIONES ATÓMICAS (SIMPLES)
+void Sumar_Puntos(int puntaje_positivo_respuesta, int* puntaje_total_usuario){
+    printf("Correcto! +%d\n", puntaje_positivo_respuesta);
+    *puntaje_total_usuario += puntaje_positivo_respuesta;
+}
+void Restar_Puntos(int puntaje_negativo_respuesta, int* puntaje_total_usuario){
+    printf("Incorrecto... -%d\n", puntaje_negativo_respuesta);
+    *puntaje_total_usuario -= puntaje_negativo_respuesta;
+
+}
+
 char Validar_Respuesta_Char(char respuesta_ingresada, char opcion_1, char opcion_2, char opcion_3, char opcion_4){
     bool respuesta_validez = false;
     while(respuesta_validez == false){
-        if(respuesta_ingresada == opcion_1 || respuesta_ingresada == opcion_2 || respuesta_ingresada == opcion_3 || respuesta_ingresada ==opcion_4 ){
+        if(respuesta_ingresada == opcion_1 || respuesta_ingresada == opcion_2 || respuesta_ingresada == opcion_3 || respuesta_ingresada == opcion_4 ){
             respuesta_validez = true;   
         }
         else{
-            printf("Respuesta inválida... Intente devuelta");
+            printf("Respuesta inválida... Intente devuelta: ");
             scanf(" %c", &respuesta_ingresada);
         }
     }
@@ -27,23 +38,20 @@ void Corregir_Respuesta(char respuesta_ingesada, int* puntaje_total_usuario, int
 
     while(respuesta_ingesada != opcion_correcta && *intentos_del_usuario < MAX_INTENTOS){
     
-        printf("Incorrecto... -%d\n", puntaje_negativo_respuesta);
-        *puntaje_total_usuario -= puntaje_negativo_respuesta;
+        Restar_Puntos(puntaje_negativo_respuesta, puntaje_total_usuario);
 
         printf("Intente devuelta: ");
         scanf(" %c", &respuesta_ingesada);
         (*intentos_del_usuario)++;
-        if( intentos_del_usuario >= MAX_INTENTOS){
-            printf("-RECHAZADO-");
+        if( *intentos_del_usuario >= MAX_INTENTOS){
+            printf("-RECHAZADO-\n");
             return;
         }
-        
     }
-    printf("Correcto! +%d\n", puntaje_positivo_respuesta);
-    *puntaje_total_usuario += puntaje_positivo_respuesta;
-
-
+    Sumar_Puntos(puntaje_positivo_respuesta, puntaje_total_usuario);
 }
+
+
 
 int main() {
     const int MAX_INTENTOS = 3;
@@ -51,23 +59,35 @@ int main() {
     char respuesta_ingresada = '.';
     bool respuesta_validez = true;
     int puntaje_total_usuario = 0;
+    int puntaje_a_sumar = 0;
+    int puntaje_a_restar = 0;
     
-        printf("¿Quién fundó realmente Springfield? \n J) \n A) \n S) \n B)");
+    //pregunta 1
+    printf("¿Quién fundó realmente Springfield? \n [J]  Jebediah Springfield \n [A] Los aliens \n [S] Los Magios \n [B] Sr. Burns\n");
+    scanf(" %c", &respuesta_ingresada);
+    puntaje_a_sumar = 100;
+    puntaje_a_restar = 20;
+    respuesta_ingresada = Validar_Respuesta_Char(respuesta_ingresada, 'J', 'A', 'S', 'B');
+    Corregir_Respuesta(respuesta_ingresada, &puntaje_total_usuario, 100,20, 'J', MAX_INTENTOS, &intentos_del_usuario);
+        
+    //pregunta 2
+    if(intentos_del_usuario < MAX_INTENTOS){
+        printf("\n¿Promete mantener en secreto la existencia de los Magios?: \n[S] Sí \n[N] No\n");
         scanf(" %c", &respuesta_ingresada);
-        respuesta_ingresada = Validar_Respuesta_Char(respuesta_ingresada, 'J', 'A', 'S', 'B');
-        Corregir_Respuesta(respuesta_ingresada, &puntaje_total_usuario, 100,20, 'J', MAX_INTENTOS, &intentos_del_usuario);
-
-        if(intentos_del_usuario <= MAX_INTENTOS){
-            printf("\n¿Promete mantener en secreto la existencia de los Magios? [S/N]: ");
-            scanf(" %c", &respuesta_ingresada);
-            bool respuesta_bool = Validar_Respuesta_Bool(respuesta_ingresada);
-            if(respuesta_bool){
-                puntaje_total_usuario += 50;
-            }
-            else{
-                puntaje_total_usuario -= 300;
-            }
+        puntaje_a_sumar = 50;
+        puntaje_a_restar = 300;
+        respuesta_validez = Validar_Respuesta_Bool(respuesta_ingresada);
+        if(respuesta_validez){
+            Sumar_Puntos(puntaje_a_sumar, &puntaje_total_usuario);
         }
+        else{
+            Restar_Puntos(puntaje_a_sumar, &puntaje_total_usuario);
+        }
+    }
+    if(intentos_del_usuario < MAX_INTENTOS){
+        printf("\n¿Cuál es su fecha de nacimiento? (formato: yyyy/mm): \n");
+        scanf(" %c", &respuesta_ingresada);
+    }
         
         
     
